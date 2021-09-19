7e6d7f5d78afa89972cd672c688d55538eb9619b
Rafael Catarino <rafaelscatarino@hotmail.com>
2021-09-19 01:44:18 -0300

    consertando um erro

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ function createRgb() {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
-  return rgb = `rgb(${r}, ${g}, ${b})`;
+  return `rgb(${r}, ${g}, ${b})`;
 }
 
 function createNewColor() {
