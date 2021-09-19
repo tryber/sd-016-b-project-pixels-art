4dfd8977d5f2d9b4d04cddf0adbcb1c879e4ea6c
Joey <joviavelino@gmail.com>
2021-09-19 01:24:37 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 window.onload = function attributeColorBlack() {
    document.getElementsByClassName("color")[0].classList.add("selected")
-    
 }
+let colorList = document.querySelectorAll(".color")
 
-function 
+function changeChoosenColor (parameter){
+    let selected = document.querySelector(".selected");
+        if(selected != null) {
+        selected.classList.remove("selected")
+        parameter.target.classList.add("selected")
+        }
+}
+function chooseColor (){
+    for(let index = 0; index < colorList.length; index++){
+        colorList[index].addEventListener("click", changeChoosenColor)
+    }
+}
+chooseColor(colorList)
