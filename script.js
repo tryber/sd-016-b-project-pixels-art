bd4ee6ba5c5d94624ec9b07be93eec00a96c9917
brunaumLL <bruno99louzada@gmail.com>
2021-09-19 20:20:59 -0300

    requisito 6 feito

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-const pixelLinha = document.getElementsByClassName('pixel-linha');
 const pixelBox = document.getElementById('pixel-board');
+const tamanhoBox = 5 * 5;
+const paletaCores = document.getElementById('color-palette');
+const listaCores = ['black', 'red', 'blue', 'green'];
+
+function recebeColor() {
+  for (let i = 0; i <= 3; i += 1) {
+    const cor = document.createElement('div');
+    cor.className = 'color';
+    cor.style.backgroundColor = listaCores[i];
+    paletaCores.appendChild(cor);
+  }
+}
+recebeColor();
+
+function selected() {
+  const cor = document.getElementsByClassName('color');
+  cor[0].className = 'color selected';
+}
+selected();
+
+/*
+function mudaCorSelected() {
+  const cor = paletaCores.childNodes;
+
+  for (let i = 0; i <= cor.length; i += 1) {
+    
+  }
+  
+}
+*/
 
 function pixelBlock() {
-  for (let index = 1; index <= 5; index += 1) {
+  for (let i = 1; i <= tamanhoBox; i += 1) {
+    const pixel = document.createElement('div');
+    pixel.className = 'pixel';
+
+    pixelBox.appendChild(pixel);
+  }
+}
+pixelBlock();
+
+/*
+for (let index = 1; index <= 5; index += 1) {
     const linha = document.createElement('div');
     linha.className = 'pixel-linha';
     for (let i = 1; i <= 5; i += 1) {
@@ -13,5 +52,4 @@ function pixelBlock() {
     }
     pixelBox.appendChild(linha);
   }
-}
-pixelBlock();
+  */
