90b2e3033a45ce02e8010e89e9693ec60ba09e4e
Joey <joviavelino@gmail.com>
2021-09-19 02:22:39 -0300

    requisito 8 e 9

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ window.onload = function attributeColorBlack() {
    document.getElementsByClassName("color")[0].classList.add("selected")
 }
 let colorList = document.querySelectorAll(".color")
+let pixelList = document.querySelectorAll(".pixel")
+let clearButton= document.getElementById("clear-board")
 
-function changeChoosenColor (parameter){
+function changeChoosenColor (event){
     let selected = document.querySelector(".selected");
         if(selected != null) {
         selected.classList.remove("selected")
-        parameter.target.classList.add("selected")
+        event.target.classList.add("selected")
         }
 }
 function chooseColor (){
@@ -16,3 +18,24 @@ function chooseColor (){
     }
 }
 chooseColor(colorList)
+
+function paintChoosenPixel (event) {
+    let selectedColor = document.querySelector(".selected")
+    let appliableColor = window.getComputedStyle(selectedColor).getPropertyValue("background-color")
+    event.target.style.backgroundColor = appliableColor
+}
+
+function choosePixel (){
+    for (let index = 0; index < pixelList.length; index++)
+    pixelList[index].addEventListener("click", paintChoosenPixel)
+}
+
+choosePixel(colorList)
+
+function clearPixels (){
+    let clearChoosenPixel = document.querySelectorAll(".pixel")
+    for (let index = 0; index < clearChoosenPixel.length; index++)
+    clearChoosenPixel[index].style.backgroundColor = "white";
+}
+
+clearButton.addEventListener("click", clearPixels)
