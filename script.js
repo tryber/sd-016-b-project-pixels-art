6fc258e2b0b0d64db2799133bfd10bab221eeb5b
Hugo Maximiano <hugomaximiano@hotmail.com>
2021-09-20 09:47:54 -0300

    passo 9

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-window.onload
 let idx = 5;
 let pixelBoard = document.getElementById("pixel-board");
+let cor =document.querySelectorAll(".color");
 
 function linha(){
    for (let index = 0; index < idx; index+=1) {
@@ -17,10 +17,50 @@ function coluna (linha){
         linha.appendChild(pixelColuna);
     }
 }
+linha();
 
-function selected (event) {
-    
-}
+function selecionarCor() {
+    let colorSelected = document.querySelector('#color-palette');
+    colorSelected.addEventListener('click', function(event){
+      let selected = document.querySelector('.selected');
+      if (event.target.id !== 'color-palette') {
+        selected.classList.remove('selected'); 
+        event.target.classList.add('selected'); 
+      }
+    });
+  }
+  selecionarCor();
+  
+  function paint () {
+   pixelBoard.addEventListener('click', function(event){
+    if (event.target.className === 'pixel') {
+      let currentColor = document.querySelector('.selected'); 
+      let changeColorPixel = event.target; 
+      changeColorPixel.style.backgroundColor = currentColor.style.backgroundColor; 
+    }
+  });
+  }
+  paint ();
+  
+  
+  function clearBoard() {
+  let buttonClear = document.getElementById('clear-board'); 
+  let pixels = document.getElementsByClassName('pixel');
+  buttonClear.addEventListener('click', function(){
+    for (let i = 0; i < pixels.length; i += 1) {   
+      pixels[i].style.backgroundColor = 'white';
+    }
+  });
+  }
+  clearBoard();
+
+let black = document.getElementById("black");
+black.style.backgroundColor = "black";
+let blue = document.getElementById("blue");
+blue.style.backgroundColor = "blue";
+let yellow = document.getElementById("yellow");
+yellow.style.backgroundColor = "yellow";
+let green = document.getElementById("green");
+green.style.backgroundColor = "green";
 
 
-linha()
