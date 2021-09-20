2b2a2be4ac2a4054062708baade7fb7612441a64
natashiaromanzini <natashiaromanzini1@gmail.com>
2021-09-20 16:42:12 -0300

    indo

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ const L4 = document.getElementById('L4');
 const L5 = document.getElementById('L5');
 
 function criacoluna (quadradao){
-
 for (i=0; i<5; i+=1){
   const coluna= document.createElement('div');
   coluna.className='pixel';
-  quadradao.appendChild(coluna)
+  quadradao.appendChild(coluna);
 }
 }
 
@@ -18,6 +17,48 @@ criacoluna(L1)
 criacoluna(L2)
 criacoluna(L3)
 criacoluna(L4)
-criacoluna(L5)]
+criacoluna(L5)
 
-let selecionacor = 
+let preto = document.querySelector('.black');
+let rosa = document.querySelector('.pink');
+const marrom = document.querySelector('.brown');
+const roxo = document.querySelector('.purple');
+
+function classSelected(event) {
+  const elementContainSelected = document.querySelector('.selected');
+  elementContainSelected.classList.remove('selected');
+  event.target.classList.add('selected');
+  console.log(elementContainSelected);
+}
+
+preto.addEventListener('click', classSelected);
+rosa.addEventListener('click', classSelected);
+marrom.addEventListener('click', classSelected);
+roxo.addEventListener('click', classSelected);
+
+function creatColor(event) {
+  const pincel = document.querySelector('.selected');
+  event.target.style.backgroundColor = window.getComputedStyle(pincel).backgroundColor;
+}
+
+function orelhao() {
+  const arrayPixel = document.getElementsByClassName('pixel');
+  for (let index = 0; index < arrayPixel.length; index += 1) {
+    const element = arrayPixel[index];
+    element.addEventListener('click', creatColor);
+  }
+}
+
+orelhao();
+
+
+function branquinho() {
+  const arrayPixel = document.getElementsByClassName('pixel');
+  for (let index = 0; index < arrayPixel.length; index += 1) {
+    const element = arrayPixel[index];
+    element.style.backgroundColor = 'white';
+  }
+}
+
+const botao = document.getElementById('clear-board');
+botao.addEventListener('click', branquinho);
