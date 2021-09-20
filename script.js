1ae843652138deeceebb0eb823f600b6d97a5b57
Izabela <izabelahammer@gmail.com>
2021-09-20 02:24:52 -0300

    todos os requisitos porem o 12 esta com erro

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// requisito 4
 // criando a matriz do quadro de pintura 5x5:
 let n = 5; // numero de linhas e colunas (matriz quadrada)
 function createMatriz (n) {
@@ -19,8 +20,77 @@ function createMatriz (n) {
  }
 } 
 createMatriz (n);  
+
+// requisito 7
+// Seleciona uma cor da paleta de cores para pintar:
+function selectedPaint() {
+  const colorSelected = document.querySelector('#color-palette');
+  colorSelected.addEventListener('click', (event) => {
+    const selected = document.querySelector('.selected');
+    if (event.target.id !== 'color-palette') {
+      selected.classList.remove('selected'); // fonte: https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList
+      event.target.classList.add('selected'); // acrescentou no que foi clicado
+    }
+  });
+}
+selectedPaint();
+// requisito 8
+// pintar pixel apos selecionar a cor e somente o pixel desejado:
+function Paint () {
+const matrixPaint = document.getElementById('pixel-board');
+matrixPaint.addEventListener('click', (event) => {
+  if (event.target.className === 'pixel') {
+    const currentColor = document.querySelector('.selected').id; //cor atual selecionada
+    const changeColorPixel = event.target; 
+    changeColorPixel.style.backgroundColor = currentColor; //muda a cor do pixel para a cor selecionada
+  }
+});
+}
+Paint ();
+// requisito 9
+// Apagar todo o quadro, deixando completamente branco:
+function clearBoard() {
+const buttonClear = document.getElementById('clear-board'); 
+const pixels = document.getElementsByClassName('pixel');
+buttonClear.addEventListener('click', () => {
+  for (let i = 0; i < pixels.length; i += 1) {   
+    pixels[i].style.backgroundColor = 'white';
+  }
+});
+}
+clearBoard();
+
+// requisito 11
+// input e botao para aumentar o quadro de pintura:
+const input = document.getElementById('board-size');
+const buttonInitial = document.getElementById('generate-board');
+// numero digitado pelo usurario é adicionado ao n
+input.addEventListener('keyup',(event) => { // Tainá me ajudou nesta parte, fonte: https://developer.mozilla.org/pt-BR/docs/Web/API/Document/keyup_event
+  n = parseInt(event.target.value,10);
+  if (n < 5) {
+    n = 5;
+  } else if ( n > 50) {
+    n = 50;
+  } 
+});
+
+// requisito 10
+//botao que muda o tamanho do quadro de pintura caso receba um valor n:
+buttonInitial.addEventListener('click', () => {
+  const myNode = document.getElementById('pixel-board');
+  while (myNode.firstChild) {
+    myNode.removeChild(myNode.lastChild);  // fonte: https://qastack.com.br/programming/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+  }
+  if (input.value === '') {
+    alert("Board inválido!");
+  } else {
+    createMatriz(n);
+  }
+});
+
 /*
-// gerar cores aletorias: 
+// requisito 12
+// gerar cores aletorias:  esta dando errado, descobrir porq...
 function gerarCorRandom() {
   const r = Math.random() * 255;
   const g = Math.random() * 255;
@@ -34,66 +104,3 @@ cor2.style.backgroundColor = gerarCorRandom();
 let cor3 = document.getElementById('blue');
 cor3.style.backgroundColor = gerarCorRandom();
 */
-
-// Seleciona uma cor da paleta de cores para pintar:
-function selectedPaint() {
-  const colorSelected = document.querySelector('#color-palette');
-  colorSelected.addEventListener('click', (event) => {
-    const selected = document.querySelector('.selected');
-    if (event.target.id !== 'color-palette') {
-      selected.classList.remove('selected'); // fonte: https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList
-      event.target.classList.add('selected'); // acrescentou no que foi clicado
-    }
-  });
-}
-selectedPaint();
-
-// pintar pixel apos selecionar a cor e somente o pixel desejado:
-function Paint () {
-const matrixPaint = document.getElementById('pixel-board');
-matrixPaint.addEventListener('click', (event) => {
-  if (event.target.className === 'pixel') {
-    const currentColor = document.querySelector('.selected').id; //cor atual selecionada
-    const changeColorPixel = event.target; 
-    changeColorPixel.style.backgroundColor = currentColor; //muda a cor do pixel para a cor selecionada
-  }
-});
-}
-Paint ();
-
-// Apagar todo o quadro, deixando completamente branco:
-function clearBoard() {
-const buttonClear = document.getElementById('clear-board'); 
-const pixels = document.getElementsByClassName('pixel');
-buttonClear.addEventListener('click', () => {
-  for (let i = 0; i < pixels.length; i += 1) {   
-    pixels[i].style.backgroundColor = 'white';
-  }
-});
-}
-clearBoard();
-
-// input e botao para aumentar o quadro de pintura:
-const input = document.getElementById('board-size');
-const buttonInitial = document.getElementById('generate-board');
-// numero digitado pelo usurario é adicionado ao n
-input.addEventListener('keyup',(event) => {
-  n = parseInt(event.target.value,10);
-  if (n < 5) {
-    n = 5;
-  } else if ( n > 50) {
-    n = 50;
-  } 
-});
-//botao que muda o tamanho do quadro de pintura caso receba um valor n:
-buttonInitial.addEventListener('click', () => {
-  const myNode = document.getElementById('pixel-board');
-  while (myNode.firstChild) {
-    myNode.removeChild(myNode.lastChild);  // fonte: https://qastack.com.br/programming/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
-  }
-  if (input.value === '') {
-    alert("Board inválido!");
-  } else {
-    createMatriz(n);
-  }
-});
