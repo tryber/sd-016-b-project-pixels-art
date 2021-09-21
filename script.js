b9b4fbb5bf6afc46bc7b0432bc93cddcbc4f9536
Eduardo Fradique <74576664+edsfradique@users.noreply.github.com>
2021-09-21 07:00:40 -0300

    C2 - requisito partial 5

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const colorPalette = document.querySelector('#color-palette');
+const mainTag = document.getElementsByTagName('main')[0];
+
 colorPalette.children[0].style.backgroundColor = 'black';
 colorPalette.children[1].style.backgroundColor = 'green';
 colorPalette.children[2].style.backgroundColor = 'red';
 colorPalette.children[3].style.backgroundColor = 'blue';
 
 colorPalette.children[0].setAttribute('class', 'color selected');
+
+// Requisito 4
+const createUl = document.createElement('ul');
+mainTag.appendChild(createUl);
+createUl.setAttribute('id', 'pixel-board');
+for (let i = 1; i <= 25; i += 1) {
+  const createLi = document.createElement('li');
+  document.getElementById('pixel-board').appendChild(createLi);
+  createLi.setAttribute('class', 'pixel');
+}
