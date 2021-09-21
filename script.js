195e572213cffd639bc91084d6ab64f0c406cb61
Eduardo Fradique <74576664+edsfradique@users.noreply.github.com>
2021-09-21 09:47:44 -0300

    C4 - requisito 7

--- script.js
+++ script.js
@@ -1,33 +1,65 @@
 const colorPalette = document.querySelector('#color-palette');
 const mainTag = document.getElementsByTagName('main')[0];
 
-colorPalette.children[0].style.backgroundColor = 'black';
-colorPalette.children[1].style.backgroundColor = 'green';
-colorPalette.children[2].style.backgroundColor = 'red';
-colorPalette.children[3].style.backgroundColor = 'blue';
+function createColor() {
+  const arrColor = ['black', 'green', 'red', 'blue'];
+  for (let i = 0; i < arrColor.length; i += 1) {
+    colorPalette.children[i].style.backgroundColor = arrColor[i];
+    if (arrColor[i] === 'black') {
+      colorPalette.children[0].setAttribute('id', 'black');
+      colorPalette.children[0].className = 'color selected';
+    } else {
+      colorPalette.children[i].setAttribute('id', arrColor[i]);
+    }
+  }
+}
 
-colorPalette.children[0].setAttribute('class', 'color selected');
+function selectColor(cor) {
+  document.querySelector('.selected').className = 'color';
+  const singleColor = cor;
+  singleColor.target.className = 'color selected';
+}
 
-// function clearBoard() {
-//   for (let i = 0; i < 25; i += 1) {
-//     document.querySelector('#pixel-board').children[i].style.backgroundColor = 'white';
-//   }
-// }
-// Requisito 9
-const createButton = document.createElement('button');
-mainTag.appendChild(createButton);
-createButton.setAttribute('id', 'clear-board');
-createButton.innerText = 'Limpar';
+function listColor() {
+  const black = document.querySelector('#black');
+  black.addEventListener('click', selectColor);
 
-// const button = document.getElementsByClassName('clear-board')[0];
-// button.addEventListener('click', clearBoard);
+  const green = document.querySelector('#green');
+  green.addEventListener('click', selectColor);
+
+  const red = document.querySelector('#red');
+  red.addEventListener('click', selectColor);
+
+  const blue = document.querySelector('#blue');
+  blue.addEventListener('click', selectColor);
+}
 
 // Requisito 4
-const createUl = document.createElement('ul');
-mainTag.appendChild(createUl);
-createUl.setAttribute('id', 'pixel-board');
-for (let i = 1; i <= 25; i += 1) {
-  const createLi = document.createElement('li');
-  document.getElementById('pixel-board').appendChild(createLi);
-  createLi.setAttribute('class', 'pixel');
+function pixelBoard() {
+  const createUl = document.createElement('ul');
+  mainTag.appendChild(createUl);
+  createUl.setAttribute('id', 'pixel-board');
+  for (let i = 1; i <= 25; i += 1) {
+    const createLi = document.createElement('li');
+    document.getElementById('pixel-board').appendChild(createLi);
+    createLi.setAttribute('class', 'pixel');
+  }
 }
+
+// Requisito 9
+function clear() {
+  for (let i = 0; i < 25; i += 1) {
+    document.getElementsByClassName('pixel')[i].setAttribute('id', '');
+  }
+}
+function clearBoard() {
+  const button = document.querySelector('#clear-board');
+  button.addEventListener('click', clear);
+}
+
+window.onload = function init() {
+  createColor();
+  listColor();
+  pixelBoard();
+  clearBoard();
+};
