e605ae1e348a6716b35fc2d82da7c22c6ea8b52e
Douglas Sabino <douglasbsabino@hotmail.com>
2021-09-22 15:51:10 -0300

    termina requisito 7

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 window.onload = function() {
 
+  function selectClass() {
+    let colors = document.getElementsByClassName('color')
+
+    for(let i = 0; i < colors.length; i++) {
+      colors[i].addEventListener('click', function(event) {
+        for(let x = 0; x < colors.length; x++) {
+          colors[x].classList.remove('selected')
+        }
+        event.target.classList.add('selected')
+      })
+    }
+  }
+
+  selectClass();
+
 
   function resetBoardPixels() {
     let pixels = document.getElementsByClassName('pixel')
     let resetButton = document.getElementById('clear-board')
 
-    resetButton.addEventListener('click', function(){
+    resetButton.addEventListener('click', function(event){
       for(let i = 0; i < pixels.length; i++) {
-        pixels[i].style.backgroundColor = 'white'
+        pixels[i].style.backgroudColor = 'white'
       }
     })
   }
